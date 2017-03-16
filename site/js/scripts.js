90596b681c042b897f4cd5a01c12530fd4126e39
Ariel Wolfe <arielwolfe@Ariels-MBP.domain>
2017-03-16 11:07:29 -0700

    first commit

---
+++ site/js/scripts.js
@@ -0,0 +1,16 @@
+		$(document).ready(function(){
+			 
+			$('.slidecara').slick({
+			dots: true,
+			infinite: true,
+			slidesToShow: 1,
+			autoplay: true,
+			autoplaySpeed: 2600,
+			fade: true, 
+			cssEase: 'linear',
+            arrows: false,
+
+			}); 
+			
+		});
+
---
+++ site/search.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  	<title>Search results</title>
+  	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">
+
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+
+    <link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+    <!-- <link rel="stylesheet" href="css/fonts.css" type="text/css" media="all"> -->
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+
+
+    <script src="js/jquery.js"></script>
+	<script src="search/search.js"></script>
+     <script src="js/jquery-migrate-1.1.1.js"></script>
+     <script src="js/superfish.js"></script>
+     <script src="js/jquery.mobilemenu.js"></script>
+
+
+     <script src="js/jquery.easing.1.3.js"></script>
+     <script src="js/scroll_to_top.js"></script>
+     <script src="js/script.js"></script>
+     <script src="js/jquery.equalheights.js"></script>
+     <script src="js/touchTouch.jquery.js"></script>
+
+
+
+
+
+     <!-- font-awesome font -->
+    <link rel="stylesheet" href="fonts/font-awesome.css" type="text/css" media="screen">
+    <!-- fontello font -->
+
+<!--[if lt IE 8]>
+     <div style=' clear: both; text-align:center; position: relative;'>
+      <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+        <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+      </a>
+    </div>
+   <![endif]-->
+   <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/ie.css">
+   <![endif]-->
+</head>
+<body>
+<!--button back top-->
+<div id="back-top"><i class="fa fa-arrow-up"></i></div>    
+<div class="main">
+  <div class="div-content content_page">  
+
+
+
+<!--==============================header=================================-->
+      <div id="stuck_container">
+        <header>
+              <div class="container">
+                <div class="row">
+                  <div class="grid_12">
+      
+                      <h1><a href="index.html"><img src="images/logo.png" alt="Plumb"></a></h1>
+                      <nav>
+                        <ul class="sf-menu header_menu">
+                          <li id="first-li"><a href="index.html">About us<strong></strong></a></li>
+                          <li><a href="index-1.html"><span></span>Most Viewed<strong></strong></a>
+                              <ul class='submenu'>
+                                <li><a href="#">Dolore ipsu</a></li>
+                                <li class='sub-menu'><a href="#">Consecte</a>
+                                   <ul class='submenu2'>
+                                      <li><a href="#">Dolore ipsu</a></li>
+                                      <li><a href="#">Consecte</a></li>
+                                      <li><a href="#">Elit Conseq</a></li>
+                                   </ul>
+                                </li>
+                                <li><a href="#">Elit Conseq</a></li>
+                              </ul>
+                           </li>
+                           <li><a href="index-2.html">Highest rated<strong></strong></a></li>
+                           <li><a href="index-3.html">Blog<strong></strong></a></li>
+                           <li id="last-li"><a href="index-4.html">Contacts<strong></strong></a></li>
+                        </ul>
+                      </nav>
+
+                    </div>
+                </div>
+              </div>
+            </header>
+      </div>
+	
+        <div class="container">
+          <div class="row">
+            <div class="grid_12">
+
+                
+                  <h2>Search result:</h2>
+                  <div id="search-results"></div>
+
+                  <div class="wrapper">
+                    <div class="indent1"></div>
+                  </div>
+                
+            </div>
+          </div>
+
+        </div>
+
+
+
+
+
+<!--=======footer=================================-->
+        
+
+    
+  </div>
+  </div>
+
+  <footer>
+        <div class="footer_priv">
+
+          <div class="container">
+
+              <ul class="soc_icons">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+              </ul>
+              <p class="txt_priv"><span class="fw600 color1">SCRIPTO </span> &copy; <span id="copyright-year"></span> | <a href="index-5.html" class="link1">Privacy Policy</a></p>
+
+
+
+            </div>
+          </div>
+      </footer>
+
+
+</body>
+</html>
